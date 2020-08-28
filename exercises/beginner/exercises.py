d559bd03206897d2e06c5e9bb021ce467186d86e
Trinity Christiana <trinity.christiana@trinity.christiana-14730>
2020-08-28 13:02:59 -0500

    added test

--- exercises/beginner/exercises.py
+++ exercises/beginner/exercises.py
@@ -1,17 +1,75 @@
 def ex_001():
     """
-    Question: Let's start with easy things first. What will the following code produce?
+    Question: Let's start with easy things first. Return what (a + a + a) will produce?
     """
     a = 2
     a = 4
     a = 6
-    # print(a + a + a)
     return 18
 
-def ex_():
+def ex_002():
     """
     """
     pass
+    # a = 1
+    # _a = 2
+    # _a2 = 3
+    # 2a = 4
+
+def ex_003():
+    """
+    """
+    pass
+    # a = 1
+    # b = 2
+    # print(a == b)
+    # print(b == c)
+
+def ex_004():
+    """
+    """
+    pass
+    #The script generates an error. Please add the appropriate code that adds variables a and b without an error.
+    #A: The script generates an error saying that an integer object cannot convert to string implicitly.
+    #Please try to convert the integer to a string explicitly then or the string to an integer.
+    # a = "1"
+    # b = 2
+    # print(a + b)
+
+def ex_005():
+    """
+    """
+    pass
+    # #Complete the script so that it prints out the second letter of the list
+    # letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    # print(letters[1])
+
+
+def ex_006():
+    """
+    """
+    pass
+    # #Complete the script so that it prints out a list containing letters d, e, f.
+    # letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    # print(letters[3:6])
+
+def ex_007():
+    """
+    """
+    pass
+    # #Complete the script so that it prints out a list containing the first three elements of list letters
+    # letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    # print(letters[:3])
+
+
+def ex_008():
+    """
+    """
+    pass
+    #Complete the script so that it prints out the letter i using negative indexing
+    # letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    # print(letters[-2])
+
 
 
 
